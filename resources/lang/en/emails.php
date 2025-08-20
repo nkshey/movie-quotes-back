d5004303293a4b5de03e923145b77e3ee7edc71c
Nkshey <peiqrishvilinika@gmail.com>
2025-08-20 20:19:29 +0400

    feat: localize emails

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'greeting'       => 'Hola',
+    'alt_text'       => "If clicking doesn't work, you can try copying and pasting it to your browser",
+    'crew_signature' => 'MovieQuotes Crew',
+
+    'verify_title'           => 'Verify your Movie Quotes email address',
+    'verify_message'         => 'Thanks for joining Movie Quotes! We really appreciate it. Please click the button below to verify your account',
+    'verify_button'          => 'Verify account',
+    'verify_support_message' => 'If you have any problems, please contact us',
+
+    'reset_title'           => 'Reset your Movie Quotes password',
+    'reset_message'         => 'We received a request to reset your password for your Movie Quotes account. Please click the button below to set a new password',
+    'reset_button'          => 'Reset password',
+    'reset_support_message' => 'If you did not request a password reset, you can safely ignore this email. If you have any problems, please contact us',
+];
